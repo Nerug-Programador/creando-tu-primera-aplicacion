f87629616a4bc687a46ee4de1274630906de617f
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-10-24 18:45:05 -0500

    Tomando decisiones #1

--- primerprograma/src/Main.java
+++ primerprograma/src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         double media = (8.2 + 6.0 + 9.0) / 3;
         System.out.println(media);
 
+        //Usando bloque de texto
         String sinopsis = """
                 Matris es una paradoja
                 la mejor pelicula de fin del milenio
@@ -17,16 +18,17 @@ public class Main {
                 """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
 
+        //Usando Casting Explícito
         int clasificacion = (int) (media / 2);
         System.out.println(clasificacion);
 
+        /*Desafío: conversión de temperatura
         int celsius = 40;
         double fahrenheitSinCasting = (celsius * 1.8) + 32;
         int fahrenheitConCasting = (int) fahrenheitSinCasting;
 
         System.out.println("La temperatura conversión de " + celsius + "C° a °F es: " + fahrenheitSinCasting + "°F");
         System.out.println("La temperatura conversión de " + celsius + "C° a °F es: " + fahrenheitConCasting + "°F");
-
+        */
     }
-
 }
