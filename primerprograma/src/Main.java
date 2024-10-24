bd2b5c6aaa538af6ecef1f0f30e7e26d985762d9
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-10-24 12:15:47 -0500

    Asignando valores a la película

--- primerprograma/src/Main.java
+++ primerprograma/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Bienbenido(a) a Screen Macth!");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPalicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);
     }
 }
