1313ace783899be5b80e1244999f69883ec62ac4
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-10-24 18:56:29 -0500

    Tomando decisiones #2

--- primerprograma/src/Decisiones.java
+++ primerprograma/src/Decisiones.java
@@ -4,11 +4,18 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPalicula = 8.2;
+        String tipoPlan = "plus";
 
         if (fechaDeLanzamiento > 2022){
             System.out.println("Péliculas más populares");
         }else {
             System.out.println("Pelicula Retro que aún vale la pena ver");
+        }
+
+        if (incluidoEnElPlan && tipoPlan.equals("plus")){
+            System.out.println("Disfrute de su pleícula");
+        }else {
+            System.out.println("Película no disponible para su plan actual");
         }
     }
 }
