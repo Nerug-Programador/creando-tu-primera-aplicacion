0c266d49684ce1eb8ef03cb6b3d16777301afdc7
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2024-10-25 14:50:56 -0500

    Desafío: juego de adivinación

--- primerprograma/src/Main.java
+++ primerprograma/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienbenido(a) a Screen Macth!");
@@ -30,5 +33,45 @@ public class Main {
         System.out.println("La temperatura conversión de " + celsius + "C° a °F es: " + fahrenheitSinCasting + "°F");
         System.out.println("La temperatura conversión de " + celsius + "C° a °F es: " + fahrenheitConCasting + "°F");
         */
+
+//Desafío: juego de adivinación
+        Scanner entradaUsuario = new Scanner(System.in);
+        int numeroAleatorio = new Random().nextInt(100) + 1; // Ajustado para estar entre 1 y 100
+        int numeroDeIntentos = 0;
+        boolean acertado = false;
+
+        System.out.println(numeroAleatorio);
+        System.out.print(String.format("""
+                **************************************************************************************
+                Adivina el número que se generó aleatoriamente,
+                este está entre 1 y 100, tendrás 5 intentos.
+                **************************************************************************************
+                """));
+
+        while (numeroDeIntentos < 5) {
+            System.out.print("Ingresa un número: ");
+            int numeroUsuario = entradaUsuario.nextInt();
+            numeroDeIntentos++;
+
+            if (numeroUsuario == numeroAleatorio) {
+                if (numeroDeIntentos == 1) {
+                    System.out.println("Felicidades, acertaste el número en tan solo " + numeroDeIntentos + " intento.");
+                    acertado = true;
+                    break;
+                } else {
+                    System.out.println("Felicidades, acertaste el número en tan solo " + numeroDeIntentos + " intentos.");
+                    acertado = true;
+                    break;
+                }
+            } else if (numeroUsuario < numeroAleatorio) {
+                System.out.println("El número es mayor");
+            } else {
+                System.out.println("El número es menor");
+            }
+        }
+
+        if (!acertado) {
+            System.out.println("Es una lástima, no lograste adivinar el número. Tal vez a la próxima.");
+        }
     }
 }
